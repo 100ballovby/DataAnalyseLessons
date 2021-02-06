6851702f7291edbfa0422a35e6a65ce9da8dde98
demidraksin <greatraksin@icloud.com>
2021-02-06 18:59:33 +0300

    Adding visulisation_data file⭐️🐋💨

--- visualize/download_csv.py
+++ visualize/download_csv.py
@@ -3,4 +3,4 @@ import pandas as pd
 download_url = 'https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv'
 
 df = pd.read_csv(download_url)
-print(type(df))
+df.plot(x='Rank', y=['P25th', 'Median', 'P75th'])
